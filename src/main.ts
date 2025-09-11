221f0be14d1fa59e1d0c99091978ac8d2388e051
Michael Langbein <michael_langbein@hotmail.de>
2025-09-11 19:19:34 +0200

    creating and deleting nodes started

--- src/main.ts
+++ src/main.ts
@@ -16,12 +16,25 @@ interface Node {
 interface Edge {
   source: Node['id'];
   target: Node['id'];
-  label: string;
 }
 
 interface Graph {
   nodes: Node[];
   edges: Edge[];
+}
+
+function isNode(node: any): node is Node {
+  return typeof node === 'object' && Object.hasOwn(node, 'id') && Object.hasOwn(node, 'value');
+}
+
+function isEdge(edge: any): edge is Edge {
+  return (
+    typeof edge === 'object' &&
+    Object.hasOwn(edge, 'source') &&
+    Object.hasOwn(edge, 'target') &&
+    typeof edge.source === 'number' &&
+    typeof edge.target === 'number'
+  );
 }
 
 const data: Graph = {
@@ -40,14 +53,24 @@ const data: Graph = {
  * State Management
  **********************************************/
 
-interface Event {
-  type: 'init' | 'createNode' | 'editNode' | 'createEdge' | 'editEdge' | 'dragNode' | 'saveState';
-  payload: any;
-}
+type Event =
+  | { type: 'init' }
+  | { type: 'selectNode'; node?: Node }
+  | { type: 'updateNode'; node: Node }
+  | { type: 'deleteNode'; node: Node }
+  | { type: 'createNode'; node: Node };
 
 interface AppState {
   data: Graph;
   selected: Node | Edge | undefined;
+}
+
+function isSelected(element: Node | Edge) {
+  if (!appState.selected) return false;
+  if (isEdge(element) && isEdge(appState.selected))
+    return element.source === appState.selected.source && element.target === appState.selected.target;
+  if (isNode(element) && isNode(appState.selected)) return element.id === appState.selected.id;
+  return false;
 }
 
 const appState: AppState = {
@@ -56,28 +79,92 @@ const appState: AppState = {
 };
 
 function updateApp(event: Event) {
+  console.log(event);
+
   // step 1: change state
   switch (event.type) {
-    case 'init':
-    case 'createEdge':
+    case 'selectNode':
+      appState.selected = event.node;
+      break;
+
+    case 'updateNode':
+      for (let i = 0; i < appState.data.nodes.length; i++) {
+        if (appState.data.nodes[i].id === event.node.id) {
+          appState.data.nodes[i] = event.node;
+        }
+      }
+      break;
+
+    case 'deleteNode':
+      appState.data.nodes = appState.data.nodes.filter((d) => d.id !== event.node.id);
+      appState.data.edges = appState.data.edges.filter((e) => e.target !== event.node.id);
+      break;
+
     case 'createNode':
-    case 'dragNode':
-    case 'editEdge':
-    case 'editNode':
+      appState.data.nodes.push(event.node);
+      break;
+
+    case 'init':
     default:
       break;
   }
 
   // step 2: given state, update app
   drawGraph(appState.data, rootSvg);
+  drawNodeForm(appState.selected);
+
+  // step 3: post effects
+  switch (event.type) {
+    case 'updateNode':
+    case 'deleteNode':
+      setTimeout(() => updateApp({ type: 'selectNode', node: undefined }));
+      break;
+    case 'createNode':
+      setTimeout(() => updateApp({ type: 'selectNode', node: event.node }));
+      break;
+    default:
+      break;
+  }
 }
 
 /**********************************************
  * Form setting and reading
  **********************************************/
 
-const nodeForm = select('#nodeForm');
-const edgeForm = select('#edgeForm');
+function drawNodeForm(selected: AppState['selected']) {
+  if (!selected || !isNode(selected)) {
+    const nodeForm = select('#nodeForm');
+    nodeForm.style('display', 'none');
+    return;
+  }
+  const nodeForm = select('#nodeForm');
+  nodeForm.style('display', 'block');
+  nodeForm.select('input[name="label"]').property('value', selected.label);
+  nodeForm.select('input[name="value"]').property('value', selected.value);
+  nodeForm.select('button.nodeUpdate').on('click', () => {
+    const newNode = { ...selected };
+    const nodeForm = select('#nodeForm');
+    newNode.label = nodeForm.select('input[name="label"]').property('value');
+    newNode.value = +nodeForm.select('input[name="value"]').property('value');
+    updateApp({ type: 'updateNode', node: newNode });
+  });
+  nodeForm.select('button.nodeDelete').on('click', () => {
+    updateApp({ type: 'deleteNode', node: selected });
+  });
+}
+
+select('#nodeCreate').on('click', () =>
+  updateApp({
+    type: 'createNode',
+    node: {
+      id: appState.data.nodes.length,
+      label: 'New node',
+      value: 1,
+      x: 0.5,
+      y: 0.5,
+    },
+  })
+);
 
 /**********************************************
  * SVG Setup
@@ -161,35 +248,26 @@ function drawGraph(graph: Graph, rootSvg: Selection<SVGSVGElement, unknown, HTML
     .exit()
     .remove();
 
-  const connectionLabel = rootSvg
-    .selectAll('.connectionLabel')
-    .data(graph.edges)
-    .enter()
-    .append('text')
-    .attr('class', 'connectionLabel')
-    .text((d) => d.label)
-    .attr('x', (d) => xScale((getNodeById(graph, d.source).x + getNodeById(graph, d.target).x) / 2))
-    .attr('y', (d) => yScale((getNodeById(graph, d.source).y + getNodeById(graph, d.target).y) / 2))
-    .exit()
-    .remove();
-
   const nodes = rootSvg
-    .selectAll('.node')
-    .data(graph.nodes)
+    .selectAll<SVGCircleElement, Node>('.node')
+    .data(graph.nodes, (d: Node) => d.id)
+    .attr('stroke', (d) => (isSelected(d) ? 'black' : 'none'))
     .enter()
     .append('circle')
     .attr('class', 'node')
     .attr('r', '10px')
     .attr('fill', 'grey')
-    .attr('outline', 'black')
+    .attr('stroke', (d) => (isSelected(d) ? 'black' : 'none'))
     .attr('cx', (d) => xScale(d.x))
     .attr('cy', (d) => yScale(d.y))
+    .on('click', (event, node) => updateApp({ type: 'selectNode', node }))
     .exit()
     .remove();
 
   const nodeLabels = rootSvg
-    .selectAll('.nodeLabel')
-    .data(graph.nodes)
+    .selectAll<SVGTextElement, Node>('.nodeLabel')
+    .data(graph.nodes, (d) => d.id)
+    .text((el) => el.label)
     .enter()
     .append('text')
     .attr('class', 'nodeLabel')
@@ -198,20 +276,10 @@ function drawGraph(graph: Graph, rootSvg: Selection<SVGSVGElement, unknown, HTML
     .attr('y', (d) => yScale(d.y))
     .exit()
     .remove();
-
-  return { nodes, nodeLabels, connections, connectionLabel };
 }
 
 /**********************************************
  * Run app
  **********************************************/
 
-updateApp({ type: 'init', payload: undefined });
-
-// allow dragging nodes
-// allow creating nodes
-// allow connecting nodes
-// allow editing nodes
-// allow editing connections
-// allow changing node-value, watch changes unfold (maybe with tapering?)
-// save changes to file
+updateApp({ type: 'init' });
